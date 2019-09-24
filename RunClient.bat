8687dd11f7014dde0fb774c77e353a90a286ba3c
notexplosive <thenotexplosive@gmail.com>
2019-09-23 21:21:59 -0700

    Check for executable before run

--- RunClient.bat
+++ RunClient.bat
@@ -1,1 +1,10 @@
-Build\DeepSwarmClient-Debug\netcoreapp3.0\DeepSwarmClient.exe %*
+@echo off
+set filename=Build\DeepSwarmClient-Debug\netcoreapp3.0\DeepSwarmClient.exe
+if exist %filename% (
+    %filename% %*
+) else (
+    echo Exe not found.
+    echo Build DeepSwarmClient as Debug first
+    echo TODO: repeat but in French
+    pause
+)
--- RunServer.bat
+++ RunServer.bat
@@ -1,1 +1,10 @@
-Build\DeepSwarmServer-Debug\netcoreapp3.0\DeepSwarmServer.exe %*
+@echo off
+set filename=Build\DeepSwarmServer-Debug\netcoreapp3.0\DeepSwarmServer.exe
+if exist %filename% (
+    %filename% %*
+) else (
+    echo Exe not found.
+    echo Build DeepSwarmServer as Debug first
+    echo TODO: repeat but in French
+    pause
+)
