f7dee3acef8eaa35f263b4ed18f52dd357480096
notexplosive <thenotexplosive@gmail.com>
2019-09-23 21:35:36 -0700

    More concise messaging

--- RunClient.bat
+++ RunClient.bat
@@ -3,8 +3,6 @@ set filename=Build\DeepSwarmClient-Debug\netcoreapp3.0\DeepSwarmClient.exe
 if exist %filename% (
     %filename% %*
 ) else (
-    echo Exe not found.
-    echo Build DeepSwarmClient as Debug first
-    echo TODO: repeat but in French
+    echo Executable not found. Build DeepSwarmClient as Debug first
     pause
 )
--- RunServer.bat
+++ RunServer.bat
@@ -3,8 +3,6 @@ set filename=Build\DeepSwarmServer-Debug\netcoreapp3.0\DeepSwarmServer.exe
 if exist %filename% (
     %filename% %*
 ) else (
-    echo Exe not found.
-    echo Build DeepSwarmServer as Debug first
-    echo TODO: repeat but in French
+    echo Executable not found. Build DeepSwarmServer as Debug first
     pause
 )
