61370ea1b14c77d6d4e2db3772e730dd814c3af2
Elisée Maurer <elisee@sparklinlabs.com>
2019-10-06 02:13:43 +0200

    Server: Don't iterate over _peersBySocket since it contains both identified and unidentified sockets, use _identifiedPeerSockets

--- DeepSwarmServer/ServerState.Playing.cs
+++ DeepSwarmServer/ServerState.Playing.cs
@@ -7,12 +7,12 @@ namespace DeepSwarmServer
     {
         void StartPlaying()
         {
-            var peers = new Peer[_peersBySocket.Count];
-            _peersBySocket.Values.CopyTo(peers, 0);
-
+            var peers = new Peer[_identifiedPeerSockets.Count];
             var players = new Game.Player[peers.Length];
+
             for (var i = 0; i < peers.Length; i++)
             {
+                peers[i] = _peersBySocket[_identifiedPeerSockets[i]];
                 peers[i].Identity.PlayerId = i;
                 players[i] = new Game.Player { Id = i, Name = peers[i].Identity.Name };
             }
@@ -27,8 +27,9 @@ namespace DeepSwarmServer
         {
             _universe.Tick();
 
-            foreach (var peer in _peersBySocket.Values)
+            foreach (var socket in _identifiedPeerSockets)
             {
+                var peer = _peersBySocket[socket];
                 var player = _universe.PlayersById[peer.Identity.PlayerId];
 
                 _packetWriter.WriteByte((byte)Protocol.ServerPacketType.Tick);
