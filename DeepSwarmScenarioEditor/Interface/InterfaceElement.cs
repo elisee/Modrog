b623a7661579597bb8b128b53e810c7db043cc8f
Elisée Maurer <elisee@sparklinlabs.com>
2019-10-17 02:15:12 +0200

    Add to parent after setting Engine member in InterfaceElement

--- DeepSwarmScenarioEditor/Interface/InterfaceElement.cs
+++ DeepSwarmScenarioEditor/Interface/InterfaceElement.cs
@@ -6,9 +6,11 @@ namespace DeepSwarmScenarioEditor.Interface
     {
         public readonly Engine Engine;
 
-        public InterfaceElement(Interface @interface, Element parent) : base(@interface.Desktop, parent)
+        public InterfaceElement(Interface @interface, Element parent)
+            : base(@interface.Desktop, null)
         {
             Engine = @interface.Engine;
+            parent?.Add(this);
         }
 
     }
