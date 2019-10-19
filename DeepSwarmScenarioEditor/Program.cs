b9f412e4ceb20cf33e76c0228791b77c0adb4f0c
Elisée Maurer <elisee@sparklinlabs.com>
2019-10-19 15:50:25 +0200

    Editor: Allow opening a scenario directly through the command line

--- DeepSwarmScenarioEditor/Program.cs
+++ DeepSwarmScenarioEditor/Program.cs
@@ -5,6 +5,13 @@
         static void Main(string[] args)
         {
             var engine = new Engine();
+
+            if (args.Length == 1)
+            {
+                var entry = engine.State.ScenarioEntries.Find(x => x.Name == args[0]);
+                if (entry != null) engine.State.OpenScenario(entry);
+            }
+
             engine.Start();
         }
     }
