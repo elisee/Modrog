9fcf41c625a919fb498c6853befd4d7e54345a33
Elisée Maurer <elisee@sparklinlabs.com>
2019-10-20 00:45:02 +0200

    Editor: Fix window covering the taskbar on startup because it was made resizable too late

--- DeepSwarmScenarioEditor/Engine.cs
+++ DeepSwarmScenarioEditor/Engine.cs
@@ -33,9 +33,8 @@ namespace DeepSwarmScenarioEditor
 
             // Rendering
             SDL.SDL_Init(SDL.SDL_INIT_VIDEO);
-            SDL.SDL_CreateWindowAndRenderer(MinimumWindowSize.X, MinimumWindowSize.Y, SDL.SDL_WindowFlags.SDL_WINDOW_MAXIMIZED, out Window, out Renderer);
+            SDL.SDL_CreateWindowAndRenderer(MinimumWindowSize.X, MinimumWindowSize.Y, SDL.SDL_WindowFlags.SDL_WINDOW_MAXIMIZED | SDL.SDL_WindowFlags.SDL_WINDOW_RESIZABLE, out Window, out Renderer);
             SDL.SDL_SetWindowTitle(Window, "DeepSwarm - Editor");
-            SDL.SDL_SetWindowResizable(Window, SDL.SDL_bool.SDL_TRUE);
             SDL.SDL_SetWindowMinimumSize(Window, MinimumWindowSize.X, MinimumWindowSize.Y);
 
             // State
